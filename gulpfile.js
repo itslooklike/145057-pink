6c7c8b005b026554b94e62e173b8e7290fdda3b7
a.masalov <delsol@bk.ru>
2016-11-30 00:30:06 +0300

    add pug to gulp, css normalize,

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,48 @@
-"use strict";
+'use strict';
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var pug = require('gulp-pug');
+var autoprefixer = require('autoprefixer');
+var server = require('browser-sync').create();
 
-gulp.task("style", function() {
-  gulp.src("sass/style.scss")
+gulp.task('style', function() {
+  gulp.src('sass/style.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer({browsers: [
-        "last 2 versions"
-      ]})
+      autoprefixer({
+        browsers: [
+          'last 2 versions'
+        ],
+        cascade: false
+      })
     ]))
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest('css'))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task('pug', function buildHTML() {
+  return gulp.src('pug/pages/*.pug')
+    .pipe(pug({
+      pretty: true,
+      basedir: 'pug'
+    }))
+    .pipe(gulp.dest('.'))
+});
+
+gulp.task('serve', ['style', 'pug'], function() {
   server.init({
-    server: ".",
+    server: '.',
     notify: false,
-    open: true,
+    open: false,
     cors: true,
     ui: false
   });
 
-  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch('sass/**/*.{scss,sass}', ['style']);
+  gulp.watch('pug/**/*.{pug,jade}', ['pug']);
+  gulp.watch('*.html').on('change', server.reload);
 });
